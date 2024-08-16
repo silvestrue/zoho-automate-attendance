9953c8094870257e61333a7f4949e811d34cb877
silvestrue <43548585+silvestrue@users.noreply.github.com>
2024-08-16 14:18:40 +0300

    Update capture cookies.py

--- capture cookies.py
+++ capture cookies.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 driver = webdriver.Chrome()
 
 # Open the Zoho People website
-driver.get("https://people.zoho.com/openlm/zp#home/dashboard")
+driver.get("https://people.zoho.com/COMPANY/zp#home/dashboard")
 
 # Add a delay to allow time for manual authentication
 time.sleep(180)  # Adjust the delay as needed (in seconds)
